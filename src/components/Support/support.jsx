3fe59f9691419051dbdb18bd6a496eaeaa6ae662
vivatsal <78379149+vivatsal@users.noreply.github.com>
2021-11-10 18:00:25 +0530

    Update support.jsx

--- src/components/Support/support.jsx
+++ src/components/Support/support.jsx
@@ -14,7 +14,7 @@ function Support(){
 
                            <h2 className="heading"> If You Are An Individual </h2>
 
-                           <form className="forms" action="mailto:vatsalviven@email.com?" encType="text/plain" method="POST">
+                           <form className="forms" action="mailto:vatsalviven@email.com?" target="_blank" encType="text/plain" method="POST">
 
                                <label className="labels">First Name: </label>
                                <input className="inputs" type="text" id="first-name" name="first name" required />
@@ -41,7 +41,7 @@ function Support(){
 
                            <h2 className="heading"> If You Are An Organisation </h2>
 
-                           <form className="forms" action="mailto:vatsalviven@email.com?" encType="text/plain" method="POST">
+                           <form className="forms" action="mailto:vatsalviven@email.com?" target="_blank" encType="text/plain" method="POST">
 
                                <label className="labels">Organisation  Name: </label>
                                <input className="inputs" type="text" id="first-name" name="first name" required />
